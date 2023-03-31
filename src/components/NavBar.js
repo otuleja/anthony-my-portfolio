8f1e494e2519abadaa273a7c56576ad222210bc5
Owen Tuleja <otuleja@Owens-MacBook-Pro.local>
2023-03-31 11:46:55 -0400

    owen's commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,28 @@ import AppBar from '@mui/material/AppBar';
 import { Link } from "react-router-dom"
 import "../styles/NavBar.css";
 import ReorderIcon from '@mui/icons-material/Reorder';;
-
 function NavBar() {
     return (
-       <div className="navbar">
-        <div className="toggleButton">
+        <div
+        // className="navbar"
+        >
+            <AppBar position="fixed">
+                <div className="links">
+                    <Link to="/"> Home </Link>
+                    <Link to="/projects"> Projects </Link>
+                    <Link to="/about"> About </Link>
+                </div>
+            </AppBar>
+            {/* <div className="toggleButton">
             <button> 
-                {" "}
-                <ReorderIcon /> </button> {" "}
+                <ReorderIcon /> </button> 
         </div>
         <div className="links">
             <Link to="/"> Home </Link>
             <Link to="/projects"> Projects </Link>
             <Link to="/about"> About </Link>
+        </div> */}
         </div>
-       </div>
     )
 }
 
